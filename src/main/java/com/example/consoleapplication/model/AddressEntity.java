2e59599315f7a72e2a89349700133ae91f826ce4
vnagarajan <vnagarajan@overstock.com>
2021-08-15 23:18:19 +0100

    created entity for person and address

---
+++ src/main/java/com/example/consoleapplication/model/AddressEntity.java
@@ -0,0 +1,36 @@
+package com.example.consoleapplication.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "address")
+public class AddressEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(name = "street_name")
+  private String street;
+
+  @Column(name = "city")
+  private String city;
+
+  @Column(name = "state")
+  private String state;
+
+  @Column(name = "postal_code")
+  private String postalCode;
+
+  @ManyToOne
+  private PersonEntity personEntity;
+}
---
+++ src/main/java/com/example/consoleapplication/model/PersonEntity.java
@@ -0,0 +1,39 @@
+package com.example.consoleapplication.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Entity
+@Builder
+@Table(name = "person")
+public class PersonEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "second_name")
+  private String secondName;
+
+  @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+  @JoinColumn(name = "person_id")
+  private final Set<AddressEntity> address = new HashSet<>();
+
+}
