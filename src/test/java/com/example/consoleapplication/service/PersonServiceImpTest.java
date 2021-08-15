83ea89ee0011f7f50b612d1ba4475b6def4512c9
vnagarajan <vnagarajan@overstock.com>
2021-08-15 23:36:47 +0100

    added unit test service layer

---
+++ src/test/java/com/example/consoleapplication/service/PersonServiceImpTest.java
@@ -0,0 +1,13 @@
+package com.example.consoleapplication.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@DisplayName("PersonService Unit Tests")
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceImpTest {
+
+
+
+}
